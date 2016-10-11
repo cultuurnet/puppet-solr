ebdd174db8608f832dd542e49fce2181eb5ad453
Kristof Willaert <kristof.willaert@cultuurnet.be>
2016-10-11 10:23:16 +0200

    Fix quoting for JAVA_OPTIONS

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,5 +3,5 @@ class solr::config {
   solr::setting { 'JETTY_USER': value   => $solr::user }
   solr::setting { 'JETTY_HOST': value   => $solr::listen_address }
   solr::setting { 'JETTY_PORT': value   => $solr::listen_port }
-  solr::setting { 'JAVA_OPTIONS': value => "-Dsolr.solr.home=/opt/solr/solr -Xmx${solr::max_heap} \$JAVA_OPTIONS" }
+  solr::setting { 'JAVA_OPTIONS': value => "\"-Dsolr.solr.home=/opt/solr/solr -Xmx${solr::max_heap} \$JAVA_OPTIONS\"" }
 }
